28f00e7274a4e2c21a86fa96588fe0305af40133
Sword <quinn@bigbrother.group>
2020-12-27 11:02:28 -0600

    Make it all work :)

---
+++ hug.router.js
@@ -0,0 +1,27 @@
+function getRandomArbitrary(max) {
+    return Math.round(Math.random() * max);
+  }
+  
+router.route('/').get((req, res) => {
+
+    var options = {method: 'GET', url: 'http://hugcdn.nyc3.digitaloceanspaces.com/'};
+
+    axios.request(options).then(function (response) {
+        const fullParsed = JSON.parse(xmlParser.toJson(response.data))
+
+        const hug = fullParsed.ListBucketResult.Contents[getRandomArbitrary(fullParsed.ListBucketResult.Contents.length)]
+        const stagedResponse = {
+        url: `https://hug.cdn.bigbrother.group/${hug.Key}`,
+        filename: hug.Key,
+        size: hug.Size
+        }
+        console.log(`A hug has been sent out to ${res.req.ip}`);
+        res.status(200).json(stagedResponse);
+
+    }).catch(function (error) {
+        console.error(error);
+    });
+
+});
+
+module.exports = router;
