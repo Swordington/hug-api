56ed5dfea0498e9813b33b8ccecab42fbbe1f235
Sword <quinn@bigbrother.group>
2020-12-31 11:27:38 -0600

    Add some documentation to the hug router

--- hug.router.js
+++ hug.router.js
@@ -1,7 +1,11 @@
+// Hug Router
+// This handles all calls to the API
+
 const router = require('express').Router();
 const axios = require('axios').default;
 const xmlParser = require('xml2json');
 
+// This is how we choose which GIF to return
 function getRandomArbitrary(max) {
     return Math.round(Math.random() * max);
   }
@@ -10,16 +14,21 @@ router.route('/').get((req, res) => {
 
     var options = {method: 'GET', url: 'http://hugcdn.nyc3.digitaloceanspaces.com/'};
 
+    // Make the API request
     axios.request(options).then(function (response) {
+        // Convert the XML response to a JS array
         const fullParsed = JSON.parse(xmlParser.toJson(response.data))
 
+        // Pick which GIF to return based off of the array
         const hug = fullParsed.ListBucketResult.Contents[getRandomArbitrary(fullParsed.ListBucketResult.Contents.length)]
         const stagedResponse = {
         url: `https://hug.cdn.bigbrother.group/${hug.Key}`,
         filename: hug.Key,
         size: hug.Size
         }
+        // Let us know that a hug has been sent out, which is very useful for knowning the activity level of the API.
         console.log(`A hug has been sent out to ${res.req.ip}`);
+        // Send back the gif and related information
         res.status(200).json(stagedResponse);
 
     }).catch(function (error) {
